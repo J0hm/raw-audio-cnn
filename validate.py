72b8c2d23360df51e87e8db61b569f9909a47f82
J0hm <johnkerr2003@gmail.com>
2023-07-28 11:16:49 -0400

    Fixed a crash when validating a GPU trained model on CPU

--- validate.py
+++ validate.py
@@ -3,10 +3,10 @@ from models import M5
 from data_loader import SCLoader
 from train import test
 
-path = "saved/m5_32_60.pt"
+path = "saved/m5_128_60.pt"
 batch_size = 256
 new_sample_rate = 8000 
-n_channel = 32
+n_channel = 128
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Running on {}".format(device))
@@ -15,8 +15,8 @@ torch.set_flush_denormal(True)
 scloader = SCLoader(device, batch_size, new_sample_rate)
 n_output = len(scloader.labels)
 model = M5(n_input=scloader.n_input, n_output=n_output, n_channel=n_channel)
-model.load_state_dict(torch.load(path))
+model.load_state_dict(torch.load(path, map_location=torch.device(device)))
 model.eval()
-print("Model loaded.")
+print("Model {} loaded.".format(path))
 
 test(model, scloader.transform, "VALIDATE", scloader.validate_loader, device, verbose=True)
