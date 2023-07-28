7711faee90ff41d1ae7e472a4221abfbcba356d2
J0hm <johnkerr2003@gmail.com>
2023-07-28 16:28:50 -0400

    Added M18 support

--- validate.py
+++ validate.py
@@ -1,12 +1,12 @@
 import torch
-from models import M5
+from models import M11
 from data_loader import SCLoader
 from train import test
 
-path = "saved/m5_128_60.pt"
+path = "saved/m11_64_60.pt"
 batch_size = 256
 new_sample_rate = 8000 
-n_channel = 128
+n_channel = 64
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Running on {}".format(device))
@@ -14,7 +14,7 @@ torch.set_flush_denormal(True)
 
 scloader = SCLoader(device, batch_size, new_sample_rate)
 n_output = len(scloader.labels)
-model = M5(n_input=scloader.n_input, n_output=n_output, n_channel=n_channel)
+model = M11(n_input=scloader.n_input, n_output=n_output, n_channel=n_channel)
 model.load_state_dict(torch.load(path, map_location=torch.device(device)))
 model.eval()
 print("Model {} loaded.".format(path))
