fa7a8dd8d1bc5094987cbb73587756292b676f65
J0hm <johnkerr2003@gmail.com>
2023-09-21 13:17:26 -0400

    Implemented working algorithm for predicting across subsets of an audio file

--- predict_file.py
+++ predict_file.py
@@ -17,37 +17,47 @@ def sample_audio(audio):
     samples = []
     while(head+sample_len<duration):
         segment = audio[head*1000:(head+sample_len)*1000]
-        samples.append(segment)
+        samparr = segment.get_array_of_samples()
+        samples.append(segment._spawn(samparr[:22050]))
         head = head+step_len
+
     return samples
 
-def get_input(samples, batch_size=32):
-    sample_arrays = [s.get_array_of_samples()[0:8000] for s in samples]
-    data = [torch.Tensor(sample_arrays[n:n+batch_size])[:, None, :] for n in range(0, len(sample_arrays)-batch_size, batch_size)]
+def get_batches(samples, batch_size=32):
+    batches = []
+    for s in samples: # TODO this is so bad need to find a better solution
+        # maybe load orignal file in pytorch and split by samples or something?
+        s.export("temp.wav", format="wav")
+        waveform, sample_rate = torchaudio.load("temp.wav")
+        batches += waveform
+    
+    batches = [torch.stack(batches[n:n+batch_size])[:,None,:] for n in range(0, len(batches)-batch_size, 32)]
+    return batches
 
-    return data
 
-
-
+def get_likely_index(output):
+    s, _ = torch.sort(output)
+    print(s)
+    return output.argmax(dim=-1)
 
 
 if __name__ == '__main__':
     mmangr = ModelManager()
 
     audio = pydub.AudioSegment.from_wav(wav_file)
-    
     samples = sample_audio(audio)
-    data = get_input(samples)
+    data = get_batches(samples)
 
-    
     #print(str(mmangr))
     print(mmangr.filter_models("marine"))
     model, loader = mmangr.load_model(20, 8000, "cpu")
     model.eval()
-    
+
     for batch in data:
-        #batch = loader.transform(data)
-        print(batch.size())
-        output = model(data)
+        batch = loader.transform(batch)
+        output = model(batch)
+        print(get_likely_index(output))
+
+    
 
 
